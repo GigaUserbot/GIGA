6b3a2b3cb6611ddc39186f98ea9601a90c300f8f
Luke <me@luuke.ml>
2022-10-02 22:43:20 +0300

    admin: add simple purge and del commands
    Signed-off-by: Luke <me@luuke.ml>

--- modules/admin.go
+++ modules/admin.go
@@ -25,9 +25,13 @@ This module provides help for the basic admin moderation rights like banning, un
 <b>Commands</b>:
  • <code>.ban `+html.EscapeString("<username/reply_to_message>")+`</code>: Use this command to ban a user.   
  • <code>.unban `+html.EscapeString("<username/reply_to_message>")+`</code>: Use this command to unban a user.   
+ • <code>.del `+html.EscapeString("<reply_to_message>")+`</code>: Use this command to delete the replied to message.   
+ • <code>.purge `+html.EscapeString("<reply_to_message>")+`</code>: Use this command to delete messages from the current one to the replied to message.   
 	`)
 	dispatcher.AddHandler(handlers.NewCommand("ban", authorised(ban)))
 	dispatcher.AddHandler(handlers.NewCommand("unban", authorised(unban)))
+	dispatcher.AddHandler(handlers.NewCommand("del", authorised(del)))
+	dispatcher.AddHandler(handlers.NewCommand("purge", authorised(purge)))
 }
 
 func ban(ctx *ext.Context, u *ext.Update) error {
@@ -85,5 +89,31 @@ func unban(ctx *ext.Context, u *ext.Update) error {
 		builder := gotgproto.Sender.Self().Edit(u.EffectiveMessage.ID)
 		builder.StyledText(ctx, text.StoArray...)
 	}
+	return dispatcher.EndGroups
+}
+
+func del(ctx *ext.Context, u *ext.Update) error {
+	chat := u.EffectiveChat()
+	reply := u.EffectiveMessage.ReplyTo.ReplyToMsgID
+	if reply == 0 {
+		ctx.DeleteMessages(chat.GetID(), []int{u.EffectiveMessage.ID})
+		return dispatcher.EndGroups
+	}
+	ctx.DeleteMessages(chat.GetID(), []int{u.EffectiveMessage.ID, reply})
+	return dispatcher.EndGroups
+}
+
+func purge(ctx *ext.Context, u *ext.Update) error {
+	chat := u.EffectiveChat()
+	reply := u.EffectiveMessage.ReplyTo.ReplyToMsgID
+	if reply == 0 {
+		ctx.DeleteMessages(chat.GetID(), []int{u.EffectiveMessage.ID})
+		return dispatcher.EndGroups
+	}
+	toDel := []int{u.EffectiveMessage.ID, reply}
+	for i := reply; i < u.EffectiveMessage.ID; i++ {
+		toDel = append(toDel, i)
+	}
+	ctx.DeleteMessages(chat.GetID(), toDel)
 	return dispatcher.EndGroups
 }
